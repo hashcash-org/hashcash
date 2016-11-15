3ff0e9dac7bdf20e84187540b2bfbcc220d92068
Andreas Redmer <a_r@posteo.de>
2016-11-16 00:19:30 +0800

    added my own implementation added / fixed unit tests

--- java/src/com/ardash/hashcash/test/HashCashTest.java
+++ java/src/com/ardash/hashcash/test/HashCashTest.java
@@ -1,12 +1,12 @@
 package com.ardash.hashcash.test;
 
-import static org.junit.Assert.*;
-
 import java.security.NoSuchAlgorithmException;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
-import com.nettgryppa.security.HashCash;
+import com.ardash.hashcash.HashCash;
 
 public class HashCashTest {
 
@@ -18,16 +18,30 @@ public class HashCashTest {
 //		hc.estimateTime(value)
 //		fail("Not yet implemented");
 	}
+	
 	@Test
 	public void verifytest() throws NoSuchAlgorithmException {
-		HashCash hc = new HashCash("1:48:110416:etienne1@cri.fr:::000A2F00000063BF012");
-//		System.out.println(hc.);
+		HashCash hc = HashCash.parseFromString("1:48:110416:etienne@cri.fr:::000A2F00000063BF012");
 		
-//		HashCash hc = 
 		System.out.println(hc);
+		System.out.println(hc.getValue());
+		System.out.println(hc.getHashSumHex());
+		System.out.println(hc.isValid());
 		
 //		hc.estimateTime(value)
 //		fail("Not yet implemented");
 	}
-	
+
+	@Test
+	public void nondeterministicMintAndVerify() {
+		for (int j = 0; j < 20; j++) {
+			for (int i = 0; i < 20; i++) {
+				HashCash hc = HashCash.mintCash("foo@bar", i,1);
+				Assert.assertTrue("garbage minted "+ hc, hc.isValid());
+			}
+		}
+//		hc.estimateTime(value)
+//		fail("Not yet implemented");
+	}
+
 }
