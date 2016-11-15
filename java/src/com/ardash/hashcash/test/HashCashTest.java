42328c962b4c41163265d49436f1ff68bfcc2d5a
ar- <altair.ibn.la.ahad.sy@gmail.com>
2016-11-15 21:04:30 +0800

    test found verification not working, all stamps accepted - library useless

--- java/src/com/ardash/hashcash/test/HashCashTest.java
+++ java/src/com/ardash/hashcash/test/HashCashTest.java
@@ -13,10 +13,21 @@ public class HashCashTest {
 	@Test
 	public void test() throws NoSuchAlgorithmException {
 		//HashCash hc = new HashCash("")
-		HashCash hc = HashCash.mintCash("foo@bar", 20);
+		HashCash hc = HashCash.mintCash("foo@bar", 20,1);
 		System.out.println(hc);
 //		hc.estimateTime(value)
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
-
+	@Test
+	public void verifytest() throws NoSuchAlgorithmException {
+		HashCash hc = new HashCash("1:48:110416:etienne1@cri.fr:::000A2F00000063BF012");
+//		System.out.println(hc.);
+		
+//		HashCash hc = 
+		System.out.println(hc);
+		
+//		hc.estimateTime(value)
+//		fail("Not yet implemented");
+	}
+	
 }
